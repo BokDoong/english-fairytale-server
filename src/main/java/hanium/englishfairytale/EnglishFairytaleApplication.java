f6dc492a71061b8181c28422da005ddfb42a96b2
이진 <ctce7226@gmail.com>
2023-09-12 23:38:49 +0900

    새 프로젝트 생성

---
+++ src/main/java/hanium/englishfairytale/EnglishFairytaleApplication.java
@@ -0,0 +1,13 @@
+package hanium.englishfairytale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishFairytaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishFairytaleApplication.class, args);
+	}
+
+}
